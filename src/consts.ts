f0059fceed846e9c098757fe27c724f90ef92415
yomna <yomna@cloudflare.com>
2026-04-11 20:14:18 -0400

    test: subsequent push to existing preview

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog (Preview Build Test)";
-export const SITE_DESCRIPTION = "Testing preview builds with the Builds API";
+export const SITE_TITLE = "Astro Blog (Preview Build Test v2)";
+export const SITE_DESCRIPTION = "Testing subsequent push to same preview";
