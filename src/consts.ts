d37e5939387ad8a74874448eccfd6f2966a173c3
yomna <yomna@cloudflare.com>
2026-04-11 20:17:07 -0400

    test: push with previews_enabled=false

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog (Preview Build Test v2)";
-export const SITE_DESCRIPTION = "Testing subsequent push to same preview";
+export const SITE_TITLE = "Astro Blog (Preview Build Test v3)";
+export const SITE_DESCRIPTION = "Push with previews_enabled=false - should NOT trigger build";
