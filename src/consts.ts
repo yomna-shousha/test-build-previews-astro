ab65092693c9bd6d5f497c3ec6925801797d10b1
yomna <yomna@cloudflare.com>
2026-04-13 15:31:26 -0400

    test: v4 preview build trigger

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog (Pipeline Live Test v3)";
-export const SITE_DESCRIPTION = "This should show as the active pipeline";
+export const SITE_TITLE = "Astro Blog (Pipeline Live Test v4)";
+export const SITE_DESCRIPTION = "Fresh preview build trigger";
 // production push test Sat Apr 11 20:21:05 EDT 2026
