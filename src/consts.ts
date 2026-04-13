9b078a55272366fbab33722cc0473b68db0d96fe
yomna <yomna@cloudflare.com>
2026-04-13 15:40:49 -0400

    test: v7

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog (Pipeline Live Test v6)";
-export const SITE_DESCRIPTION = "v6 preview build trigger";
+export const SITE_TITLE = "Astro Blog (Pipeline Live Test v7)";
+export const SITE_DESCRIPTION = "v7 - with scriptTag prop fix";
 // production push test Sat Apr 11 20:21:05 EDT 2026
